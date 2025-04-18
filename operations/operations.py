6294eb50c090469c9c219eb1362438f690fc4cfe
philkasumbi <philkasumbi@gmail.com>
2025-04-18 18:17:13 +0300

    operations and expressions in python
    learned expressions and operations

---
+++ operations/operations.py
@@ -0,0 +1,18 @@
+ #an expression is a code that returns a value
+a  = 5
+b = 10
+
+
+#logical operators
+print(a>b)#grater than 
+print(a<b)#less than 
+print(a>=b)#grater/equal to
+print(a<=b)#less/equal to
+
+#assignment opeartors
+
+score = 10 #assign
+score += 5 #add and reassign
+score -= 7 #subtract and reassign
+
+print(score)
